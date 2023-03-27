231d3f7df26d4abac88bc93059e9848187010608
Yubie <ayoub04bd@gmail.com>
2023-03-27 00:05:10 +0000

    added routes

--- resources/js/Layouts/AuthLayout.vue
+++ resources/js/Layouts/AuthLayout.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="h-screen md:flex">
 	<div class="relative overflow-hidden md:flex w-1/2 bg-gradient-to-tr from-blue-500 to-blue-700 i justify-around items-center hidden">
-		<div>
-			<h1 class="text-white font-bold text-4xl font-sans">NAJA7I</h1>
+		<div class="text-center">
+			<h1 class="text-white font-bold text-4xl font-sans"><span class="underline">N</span>AJA7I</h1>
 			<p class="text-white mt-1">We believe in Success !</p>
-			<Link class="text-center block w-28 bg-white text-blue-600 mt-4 py-2 rounded-2xl hover:bg-gray-300  font-bold mb-2 transition duration-700 ease-in-out" href="">Read More</Link>
+			<Link class="text-center block w-29 bg-white text-blue-600 mt-4 py-2 rounded-2xl hover:bg-gray-300  font-bold mb-2 transition duration-700 ease-in-out" :href="route('home')">Explore More</Link>
         </div>
 		<div class="absolute -bottom-32 -left-40 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
 		<div class="absolute -bottom-40 -left-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
