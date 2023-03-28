daca45e919ecaeaabe2533f5f97033d653a7f30f
Yubie <ayoub04bd@gmail.com>
2023-03-28 06:30:48 +0000

    changed pagination color

--- resources/js/Components/UI/Pagination.vue
+++ resources/js/Components/UI/Pagination.vue
@@ -6,7 +6,7 @@
       v-for="(link, index) in links" 
       :key="index" class="py-2 px-4 rounded-md" 
       :href="link.url" 
-      :class="{'bg-indigo-500 dark:bg-indigo-800 text-gray-300': link.active}"
+      :class="{'bg-blue-600 dark:bg-indigo-800 text-gray-300': link.active}"
       v-html="link.label"
     />
 
