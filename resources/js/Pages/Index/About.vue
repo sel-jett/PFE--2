aaf2aafa2dec012b01ccc34eaf5ea0f3b8120585
Yubie <ayoub04bd@gmail.com>
2023-03-28 06:32:13 +0000

    about us paragraph

--- resources/js/Pages/Index/About.vue
+++ resources/js/Pages/Index/About.vue
@@ -1,5 +1,18 @@
 <template>
-
+<div class="m-auto w-1/2 my-8 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+        <h5 class="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Who are we?</h5>
+    </a>
+    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        Welcome to <span class="text-blue-600">Naja7i</span>, your one-stop destination for online courses! Our website was created by two young and ambitious individuals who wanted to make high-quality education accessible to everyone. At <span class="text-blue-600">Naja7i</span>, we believe that everyone should have access to the best teachers and courses, regardless of their location or financial situation.
+        <br>
+That's why we have curated a team of the best teachers in Morocco to create engaging and informative online courses that cover a wide range of subjects. Our courses are designed to be accessible to learners of all levels, from beginners to advanced learners. We believe that everyone should have the opportunity to learn and grow, and we are committed to making that possible through our platform.
+        <br>
+At <span class="text-blue-600">Naja7i</span>, we are dedicated to providing a seamless and user-friendly experience for our learners. We understand that online learning can be challenging, which is why we have designed our courses to be interactive, engaging, and easy to follow. Whether you're looking to improve your skills for personal or professional development, we have a course that will meet your needs.
+        <br>
+Thank you for choosing <span class="text-blue-600">Naja7i</span> as your online learning platform. We are excited to have you join our community of learners and are committed to providing you with the best learning experience possible.
+    </p>
+</div>
 </template>
 
 <script setup>
