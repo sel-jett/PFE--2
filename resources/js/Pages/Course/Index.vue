34cfd18d0a84890d5df875176ee4d5b5b2f0a7b0
Yubie <ayoub04bd@gmail.com>
2023-03-28 06:31:38 +0000

    added style

--- resources/js/Pages/Course/Index.vue
+++ resources/js/Pages/Course/Index.vue
@@ -1,17 +1,19 @@
 <template>
-  <Filters :filters="filters"/>
-  <div v-for="course in courses.data" :key="course.id" :course="course">
-    <div>
+  <Filters :filters="filters" class="mb-8"/>
+  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+    <div v-for="course in courses.data" :key="course.id" :course="course" class="bg-white rounded-lg shadow-md overflow-hidden">
+      <div class="p-4">
         <Link :href="route('course.show',{course: course.id})">
-            <CourseAdresse :course="course" />
+          <CourseAdresse :course="course" class="text-gray-700 font-medium hover:text-indigo-600"/>
         </Link>
+      </div>
     </div>
   </div>
-<div v-if="courses.data.length" class="w-full flex justify-center mt-8 mb-8">
-    <Pagination :links="courses.links" />
+  <div v-if="courses.data.length" class="w-full flex justify-center mt-8 mb-8">
+    <Pagination :links="courses.links" class="flex items-center space-x-2"/>
   </div>
-
 </template>
+
 
 <script setup>
 import {Link} from '@inertiajs/inertia-vue3'
