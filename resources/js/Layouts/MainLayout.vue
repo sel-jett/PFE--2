3bf83dffed7e316e3cc00a04392099e5fc881f00
Yubie <ayoub04bd@gmail.com>
2023-03-28 07:41:09 +0000

    changed some style

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,5 +1,5 @@
 <template>
-
+<!-- header -->
 <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2 shadow rounded dark:bg-gray-900">
   <div class="container flex flex-wrap items-center justify-between mx-auto">
   <Link :href="route('home')" class="flex items-center">
@@ -42,22 +42,23 @@
   </div>
   <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
     <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-      <li>
-        <Link :href="route('home')" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page" :class="{ 'text-blue-500 active': activeLink === 'home' }" @click="setActiveLink('home')">Home</Link>
-      </li>
-      <li>
-        <Link :href="route('about-us')" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" :class="{ 'text-blue-500 active': activeLink === 'about-us' }" @click="setActiveLink('about-us')">About us</Link>
-      </li>
-      <li>
-        <Link :href="route('course.index')" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" :class="{ 'text-blue-500 active': activeLink === 'course.index' }" @click="setActiveLink('course.index')">Courses</Link>
-      </li>
-    </ul>
+  <li :class="{ 'text-blue-500': activeLink === 'home' }">
+    <Link :href="route('home')" class="block py-2 pl-3 pr-4 text-gray-700 bg-blue-700 rounded md:bg-transparent md:p-0 hover:text-blue-700 dark:text-white" aria-current="page" @click="setActiveLink('home')">Home</Link>
+  </li>
+  <li :class="{ 'text-blue-500': activeLink === 'about-us' }">
+    <Link :href="route('about-us')" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" @click="setActiveLink('about-us')">About us</Link>
+  </li>
+  <li :class="{ 'text-blue-500': activeLink === 'course.index' }">
+    <Link :href="route('course.index')" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" @click="setActiveLink('course.index')">Courses</Link>
+  </li>
+</ul>
+
   </div>
   </div>
 </nav>
 <slot>default</slot>
 
-
+<!-- footer -->
 <footer class="bg-gray-100 dark:bg-gray-900">
     <div class="mx-auto w-full container p-4 sm:p-6">
         <div class="md:flex md:justify-between">
@@ -157,7 +158,7 @@ export default {
   data() {
     return {
       showDropdown: false,
-	  activeLink: 'home',
+	    activeLink: 'about-us',
     };
   },
   methods: {
