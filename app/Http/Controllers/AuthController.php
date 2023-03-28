47840a3aad7c6a95f4519061ea424ff36c3e9ce0
Yubie <ayoub04bd@gmail.com>
2023-03-28 00:26:55 +0000

    fixed a route

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -26,7 +26,7 @@ class AuthController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended('/course');
+        return redirect()->intended('/');
     }
 
     public function destroy(Request $request)
