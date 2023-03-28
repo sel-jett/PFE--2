e1e49542fd6d0d836ee7dfbd60c7817eee3225e9
Yubie <ayoub04bd@gmail.com>
2023-03-28 07:41:32 +0000

    changed the title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Larazillow</title>
+    <title>NAJA7I</title>
     @routes
     @vite('resources/js/app.js')
     @inertiaHead
