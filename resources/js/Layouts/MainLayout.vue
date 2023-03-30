39ca28e6ec0fb428d6e9e325b067f5908fdf5b12
Yubie <ayoub04bd@gmail.com>
2023-03-30 06:47:43 +0000

    added faq to navbar

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -70,6 +70,9 @@
   <li :class="{ 'text-blue-500': activeLink === 'course.index' }">
     <Link :href="route('course.index')" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" @click="setActiveLink('course.index')">Courses</Link>
   </li>
+  <li :class="{ 'text-blue-500': activeLink === 'course.index' }">
+    <Link :href="route('faq')" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" @click="setActiveLink('faq')">FAQ</Link>
+  </li>
 </ul>
 
   </div>
