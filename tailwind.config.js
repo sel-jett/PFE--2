4392f60e39b335896d99f90cda422347609e93df
Yubie <ayoub04bd@gmail.com>
2023-03-30 07:12:09 +0000

    added dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
+  darkMode: 'media',
   content: [
     './storage/framework/views/*.php',
     './resources/views/**/*.blade.php',
