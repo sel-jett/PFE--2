8000f072e8139b4f43bc60957a2fca9e20f8249a
Yubie <ayoub04bd@gmail.com>
2023-03-31 00:17:16 +0000

    added style to courseadresse

--- resources/js/Components/CourseAdresse.vue
+++ resources/js/Components/CourseAdresse.vue
@@ -1,7 +1,19 @@
 <template>
-  <span>
-    {{ course.Course_name }}, {{ course.category }}, for ${{ course.Price }}
-  </span>
+  <div class="flex font-bold justify-between">
+    <div class="">
+      {{ course.category }}
+    </div>
+    <div class="text-right">
+      {{ course.Price }}<span class="text-green-600"> MAD</span>
+    </div>
+  </div>
+  <div class="border-b my-2"></div>
+  <div class="font-medium">
+    {{ course.Course_name }}
+  </div>
+  <div class="text-gray-600 italic text-sm">
+    {{ course.Description }}
+  </div>
 </template>
 
 <script setup>
